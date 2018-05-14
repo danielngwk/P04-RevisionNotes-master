9890ac4ddc7520eb9ab6705ecc77bde87057d814
16022653 <16022653@myrp.edu.sg>
2018-05-14 16:43:46 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
@@ -0,0 +1,71 @@
+package com.myapplicationdev.android.p04_revisionnotes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
+    Context context;
+    ArrayList<Note> notes;
+    int resource;
+    ImageView iv1, iv2, iv3, iv4, iv5;
+    TextView tvContent;
+
+    public RevisionNotesArrayAdapter(Context context, int resource, ArrayList<Note> notes) {
+        super(context, resource, notes);
+        this.context = context;
+        this.notes = notes;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(resource, parent, false);
+
+        tvContent = rowView.findViewById(R.id.textViewNote);
+        iv1 = rowView.findViewById(R.id.imageView1star);
+        iv2 = rowView.findViewById(R.id.imageView2star);
+        iv3 = rowView.findViewById(R.id.imageView3star);
+        iv4 = rowView.findViewById(R.id.imageView4star);
+        iv5 = rowView.findViewById(R.id.imageView5star);
+        Note note = notes.get(position);
+
+        //Check if the property for starts >= 5, if so, "light" up the stars
+        if (note.getStars() == 5) {
+            iv5.setImageResource(android.R.drawable.btn_star_big_on);
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (note.getStars() == 4) {
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (note.getStars() == 3) {
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (note.getStars() == 2) {
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (note.getStars() == 1) {
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        }
+
+        tvContent.setText(note.getNoteContent() + "");
+
+        return rowView;
+    }
+
+
+}
